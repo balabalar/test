b4b925ed76d2ce967a07a47fa266a688980845c8
balabalar <1420185269@qq.com>
2020-07-21 00:58:24 +0800

    约瑟夫问题（添加多态）

--- JosephusCycle.py
+++ JosephusCycle.py
@@ -1,54 +1,93 @@
-import file_reader
 import zipfile
 import csv
 import chardet
-import os
+import os 
 
-def get_people_list_txt(place):#获得初始游戏者列表
-    with open(place) as fp: 
-        people_str = fp.readlines()
-    people_list = []
-    for line in people_str:
-        line = list(line.strip().split(' '))
-        people_list.append(line)
-    return (people_list)
+class read(object):
+    def get_people_list(self):
+        raise NotImplementedError
 
-def get_people_list_zip(place):
-    people_list = []
-    with zipfile.ZipFile(place, 'r') as zf: 
-        first_file_name = zf.namelist()[0]
-        people_betye = zf.read(first_file_name)
-        people_str = people_betye.decode(chardet.detect(people_betye)['encoding'])#解码 
-    temp = list(people_str.strip().split('\r'))
-    for x in temp:
-        x = list(x.strip().split(' '))
-        people_list.append(x)
-    return (people_list)
+class txt_reader(read):
+    def __init__(self, path):
+        self.path = path
 
-def get_people_list_csv(place):
-    people_list = []
-    with open(place) as csvfile: 
-        readcsv = csv.reader(csvfile, delimiter = ',')
-        for row in readcsv:
-            people_list.append(row)
-    return (people_list)
+    def get_people_list(self):
+        with open(self.path) as fp: 
+            people_str = fp.readlines()
+        people_list = []
+        for line in people_str:
+            line = list(line.strip().split(' '))
+            people_list.append(line)
+        return (people_list)
 
-def get_people_list(path):
+class csv_reader(read):
+    def __init__(self, path):
+        self.path = path
+
+    def get_people_list(self):
+        people_list = []
+        with open(self.path) as csvfile: 
+            readcsv = csv.reader(csvfile, delimiter = ',')
+            for row in readcsv:
+                people_list.append(row)
+        return (people_list)
+
+class zip_reader(read):
+    def __init__(self, path):
+        self.path = path
+
+    def get_people_list(self):
+        people_list = []
+        with zipfile.ZipFile(self.path, 'r') as zf: #打开zip中的.txt文件
+            first_file_name = zf.namelist()[1]
+            people_betye = zf.read(first_file_name)
+            people_str = people_betye.decode(chardet.detect(people_betye)['encoding'])#解码 
+        temp = list(people_str.strip().split('\r'))
+        for x in temp:
+            x = list(x.strip().split(' '))
+            people_list.append(x)
+        return (people_list)
+'''
+    def get_people_list(self):  #打开zip中的.csv文件
+        people_list = []
+        with zipfile.ZipFile(place, 'r') as zf: 
+            first_file_name = zf.namelist()[0]
+            people_betye = zf.read(first_file_name)
+            people_str = people_betye.decode(chardet.detect(people_betye)['encoding'])#解码 
+        temp = list(people_str.replace('\r','').split('\n'))
+        for x in temp:
+            x = list(x.strip().split(','))
+            people_list.append(x)
+        return (people_list)
+'''
+
+def reader(path):
     file = os.path.splitext(path)
     filename, type = file
     if type == '.txt':
-        return get_people_list_txt(path)
+        return txt_reader(path).get_people_list()
     elif type == '.csv':
-        return get_people_list_csv(path)
+        return csv_reader(path).get_people_list()
     elif type == '.zip':
-        return get_people_list_zip(path)
+        return zip_reader(path).get_people_list()
     else:
         pass
-class Person():
+
+
+class person():
     def __init__(self, name, gender, age):
         self.name = name
         self.gender = gender
         self.age = age
+
+def get_person_list(gamer_list):#获得对象列表
+    person_list = Josephus()
+    for i in (range(len(gamer_list))):#行数
+            #j = len(people_list[0])#列数
+        temp = person(gamer_list[i][0], gamer_list[i][1], gamer_list[i][2])
+            #print(temp.age)
+        person_list.append(temp)
+    return person_list
 
 class Josephus():
     def __init__(self):
@@ -70,20 +109,8 @@ class Josephus():
             if start_num == -1:
                 start_num = 0
 
-        #return (self.people[0])
-
-def get_person_list(gamer_list):#获得对象列表
-    person_list = Josephus()
-    for i in (range(len(gamer_list))):#行数
-            #j = len(people_list[0])#列数
-        temp = Person(gamer_list[i][0], gamer_list[i][1], gamer_list[i][2])
-            #print(temp.age)
-        person_list.append(temp)
-    return person_list
-
 if __name__ == '__main__':
-    #people_list = getdadaForJC1.get_people_list('py.txt')
-    people_list = get_people_list('py.txt')
+    people_list = reader('py.txt')
     person_list = get_person_list(people_list)
     start_num = int(input('the start number is:'))
     interval = int(input('the interval is:'))
@@ -94,10 +121,9 @@ if __name__ == '__main__':
     print('survival ==> name:',survival.name, 'gender:',survival.gender, 'age:',survival.age)
 
 
-'''
-import JosephusCycle1  #测试文件
+#*************约瑟夫类测试***********************
 def test(people_list, start_num, interval):
-    person_list = JosephusCycle1.get_person_list(people_list)
+    person_list = get_person_list(people_list)
     killed_person = person_list.get_killed_person(start_num, interval)
     killed_list = []
     for x in killed_person:
@@ -105,10 +131,21 @@ def test(people_list, start_num, interval):
     #survival = person_list.people[0]
     return killed_list
 
-
 people_list = [[1,2,3],[4,5,6],[7,8,9]]    
-start_num = int(input('the start number is:'))
-interval = int(input('the interval is:'))
-dead_list = test(people_list, start_num, interval)
-assert(dead_list == [4,1])
-'''
+dead_list1 = test(people_list, -1, 1)
+assert(dead_list1 == [7,1]) #start_num = -1，interval = 1
+dead_list2 = test(people_list, 1, -1)
+assert(dead_list2 == [7,1]) #start_num = -1，interval = -1
+
+#*************读取文件reader类测试**************
+people_list0 = reader('py.txt')
+assert people_list0[0][1] == '女'
+assert people_list0[44][2] == '34'
+
+people_list1 = reader('py.csv')
+assert people_list1[0][0] == '妮妮'
+assert people_list1[9][2] == '25'
+
+people_list3 = reader('people.zip')
+assert people_list3[0][1] == '女'
+assert people_list3[44][2] == '34'
